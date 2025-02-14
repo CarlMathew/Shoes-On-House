30475eb398655b3ce1033d3567b19711399ef4ce
CarlMathew <moradacarl2711@gmail.com>
2025-02-14 20:52:06 +0800

    fixing the bg picture

--- src/Body.tsx
+++ src/Body.tsx
@@ -1,4 +1,4 @@
-import Logo from "./assets/logo.png"
+import Logo from "/assets/logo.png"
 import "./Body.css"
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { useRef, useState } from "react"
